043149f0a20d48943b6dae96918df3252223081d
nmckimm <35746958+nmckimm@users.noreply.github.com>
2018-02-23 14:00:04 +0000

    Removed debugtoolbar

--- dummyapp/app/views.py
+++ dummyapp/app/views.py
@@ -1,9 +1,6 @@
 from flask import render_template
 from app import app
-from flask_debugtoolbar import DebugToolbarExtension
-app.debug = True
-app.config['SECRET_KEY'] = '123'
-toolbar = DebugToolbarExtension(app)
+
 import importlib.util
 spec = importlib.util.spec_from_file_location("main", "/home/neil/git/COM30670/Assignment2/systeminfo/systeminfo/main.py")
 foo = importlib.util.module_from_spec(spec)
